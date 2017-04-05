2873c05c5129adbabfd26877ceeda0b952ae03ba
Francis Sabado <fs8080@gmail.com>
2017-04-05 08:10:42 -0500

    Edited the way animations worked. Made the site more responsive and shows them better.

--- js/main.js
+++ js/main.js
@@ -1,42 +1,43 @@
-$(document).ready( function() {
+$(document).ready(function() {
 
-	// Logo
-	var $logo 	= $('#logo');
-	 if (location.href.indexOf("#") != -1) {
-        if(location.href.substr(location.href.indexOf("#"))!='#about'){
-        	$logo.show();
+    // Logo
+    var $logo = $('#logo');
+    if (location.href.indexOf("#") != -1) {
+        if (location.href.substr(location.href.indexOf("#")) != '#about') {
+            $logo.show();
         }
     }
-    
-	// Show logo 
-	$('#tab-container .tab a').click(function() {
-	  $logo.slideDown('slow');
-	});
-	// Hide logo
-	$('#tab-about').click(function() {
-	  $logo.slideUp('slow');
-	});	
-function animMeter(){
-    $(".meter > span").each(function() {
-                $(this)
-                    .data("origWidth", $(this).width())
-                    .width(0)
-                    .animate({
-                        width: $(this).data("origWidth")
-                    }, 1200);
-            });
-}
-animMeter();
 
-      $('#tab-container').easytabs({
-        animate			: true,
-        updateHash		: true,
-        transitionIn	: 'slideDown',
-        transitionOut	: 'slideUp',
-        animationSpeed	: 600,
-        tabActiveClass	: 'active'}).bind('easytabs:midTransition', function(event, $clicked, $targetPanel){
-            if($targetPanel.selector=='#resume'){
-                    animMeter();
-            }
-        });
+    // Show logo
+    $('#tab-container .tab a').click(function() {
+        $logo.slideDown('fast');
     });
+    // Hide logo
+    $logo.slideUp('fast');
+    $('#tab-about').click(function() {});
+
+    function animMeter() {
+        $(".meter > span").each(function() {
+            $(this)
+                .data("origWidth", $(this).width())
+                .width(0)
+                .animate({
+                    width: $(this).data("origWidth")
+                }, 600);
+        });
+    }
+    animMeter();
+
+    $('#tab-container').easytabs({
+        animate: false,
+        updateHash: true,
+        transitionIn: 'slideDown',
+        transitionOut: 'slideUp',
+        animationSpeed: 200,
+        tabActiveClass: 'active'
+    }).bind('easytabs:midTransition', function(event, $clicked, $targetPanel) {
+        if ($targetPanel.selector == '#resume') {
+            animMeter();
+        }
+    });
+});
